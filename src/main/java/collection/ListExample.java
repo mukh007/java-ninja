40750fd8214a23ec32bedeeefab9cbcde5c15404
Mukesh Jha <mukesh.jha@oracle.com>
2018-05-14 10:59:55 -0600

    example

--- src/main/java/collection/ListExample.java
+++ src/main/java/collection/ListExample.java
@@ -1,7 +1,27 @@
 package collection;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ListExample {
     public static void main(String[] args) {
+        callListExample();
+        callMapExample();
+    }
+
+    private static void callMapExample() {
+        Map hashmap = new HashMap<String, String>();
+        hashmap.put("zuhi", "baklol");
+        hashmap.put("mj", "smart");
+
+        System.out.println("This is hash map example code");
+        String[] keys = {"zuhi", "mj"};
+        for (String key : keys) {
+            System.out.println(key + " is " + hashmap.get(key));
+        }
+    }
+
+    private static void callListExample() {
         System.out.println("This is array list example code");
     }
 }
